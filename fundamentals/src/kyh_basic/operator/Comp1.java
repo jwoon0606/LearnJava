15696b3e8d7ce3b596b1cbdb29457b62d24926cd
jwoon0606 <jwoon0606@gmail.com>
2024-06-13 04:21:08 +0900

    Add codes

---
+++ fundamentals/src/kyh_basic/operator/Comp1.java
@@ -0,0 +1,19 @@
+package kyh_basic.operator;
+
+public class Comp1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+        // 결과를 boolean 변수에 담음
+        boolean result = false;
+        System.out.println(result);
+    }
+}
---
+++ fundamentals/src/kyh_basic/operator/Operator2.java
@@ -0,0 +1,31 @@
+package kyh_basic.operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        // 문자열과 문자열 더하기 1
+        String result1 = "hello " + "world";
+        System.out.println(result1);
+
+        // 문자열과 문자열 더하기2
+        String s1 = "sring1";
+        String s2 = "string2";
+        String result2 = s1 + s2;
+        System.out.println(result2);
+
+        // 문자열과 숫자 더하기1
+        String result3 = "a + b = " + 10;
+        System.out.println(result3);
+
+        // 문자열과 숫자 더하기2
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = str + num;
+        System.out.println(result4);
+
+        // 문자열과 불리언 더하기
+        boolean bool = true;
+        String str1 = "bool = ";
+        String result5 = str1 + bool;
+        System.out.println(result5);
+    }
+}
