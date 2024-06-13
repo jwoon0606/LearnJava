1b7689f643d166965ab69143aa5baeb753340913
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-06-13 19:36:04 +0900

    [Bronze V] Title: A-B, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        int min = num1 - num2;
+
+        bw.write(String.valueOf(min));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
